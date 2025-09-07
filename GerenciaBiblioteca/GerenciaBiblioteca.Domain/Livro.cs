95fa2983ac147ed92898eb9047b42b4067ee00f5
BeyondBirthd4y <fehrnandes.gabriel@gmail.com>
2025-09-07 15:18:57 -0300

    primeiro commit de projeto

---
+++ GerenciaBiblioteca/GerenciaBiblioteca.Domain/Livro.cs
@@ -0,0 +1,9 @@
+﻿namespace GerenciaBiblioteca.Domain
+{
+    public class Livro
+    {
+        public string Isbn { get; set; }
+        public string Titulo { get; set; }
+        public string Autor {  get; set; }
+    }
+}
